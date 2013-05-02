7f7e51a2bd60e941e790dcb6fe5a8e5521bf25dd
wmyszke <w.myszke@gmail.com>
2013-05-02 20:35:03 +0200

    zmiany_rozne_001

--- MvcFilmoteka/Controllers/HomeController.cs
+++ MvcFilmoteka/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcFilmoteka.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Witaj w portalu Filmoteka!";
 
             return View();
         }
